05a7f043296b819056705f70a55dc3de5b155eec
Maor Magori <maor@magori.online>
2021-10-12 11:46:58 +0300

    renamed getBook and added validation of undefined handlers.

--- src/server/worker.ts
+++ src/server/worker.ts
@@ -1,11 +1,11 @@
 import { manifest, book } from "../types";
 
 type searchHandler = (textToSearch: string) => Promise<book[]>;
-type getBookHandler = (requestedBook: book) => Promise<book>;
+type populateBookHandler = (requestedBook: book) => Promise<book>;
 
 export type workerHandlers = {
     searchHandler?: searchHandler;
-    getBookHandler?: getBookHandler;
+    populateBookHandler?: populateBookHandler;
 };
 
 class worker {
@@ -16,16 +16,22 @@ class worker {
         this.manifest = Object.freeze(workerManifest);
         this.handlers = handlers;
     }
+    validateHandlers() {
+        if (this.handlers.populateBookHandler !== undefined)
+            throw new Error("getBook handler is undefined");
+        if (this.handlers.searchHandler !== undefined)
+            throw new Error("search handler is undefined");
+    }
 
     setSearchHandler(handlerFunction: searchHandler): worker {
         this.handlers.searchHandler = handlerFunction;
         return this;
     }
 
-    setGetBookHandler(handlerFunction: getBookHandler): worker {
-        this.handlers.getBookHandler = handlerFunction;
+    setGetBookHandler(handlerFunction: populateBookHandler): worker {
+        this.handlers.populateBookHandler = handlerFunction;
         return this;
     }
 
-    //TODO: add handlers and manifest validation
+    start() {}
 }
